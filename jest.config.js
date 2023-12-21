4c04a9c9a9aec39c64a63fe5905ba870cf8048fb
prog <email@gmail.com>
2023-12-21 11:54:08 +0700

    Navigation testing

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
 module.exports = {
   preset: 'react-native',
+  // moduleFileExtensions: ["js", "mjs", "cjs", "jsx", "ts", "tsx", "json", "node"]
+  setupFiles: ['./__mocks__/rn-gesture-handler.js'],
 };
